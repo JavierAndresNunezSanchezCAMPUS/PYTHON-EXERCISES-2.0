b6cd075bd345ae20d25a79ac8e43c0b76b570650
JavierAndresNunezSanchezCAMPUS <123566109+JavierAndresNunezSanchezCAMPUS@users.noreply.github.com>
2023-02-16 17:18:11 -0500

    Ejercicio2

--- SKILLS-REVIEW-2/app.py
+++ SKILLS-REVIEW-2/app.py
@@ -1,107 +1,43 @@
 """
-1. Campus requiere administrar algunos datos de sus Campers
-como por ejemplo, la creación, eliminación o búsqueda de los
-developers, entre otros, por tal razón, ha solicitado el diseño de
-un programa que cuente con el siguiente menú como panel de
-control:
+2. N atletas han pasado a finales en salto triple en los juegos
+olímpicos de 2022.
+
+Diseñe un programa que pida por teclado los nombres de cada
+atleta finalista y a su vez, sus marcas del salto en metros.
+
+Informar el nombre de la atleta campeona que se quede
+con la medalla de oro y si rompió récord, reportar el pago que
+será de 500 millones. El récord esta en 15,50 metros.
 """
 
-print("-----------------------MENU-----------------------")
-print("1. CREAR GRUPO ARTEMIS")
-print("1.1 LISTAR CAMPERS DE ARTEMIS")
-print("1.2 AGREGAR CAMPERS A ARTEMIS")
-print("1.3 ELIMINAR CAMPERS DE ARTEMIS")
-print("1.4 ORDENAR ALFABETICAMENTE EN LISTA DE ARTEMIS")
-print("1.5 BUSCAR CAMPER EN LISTA DE ARTEMIS")
-print("2. CREAR GRUPO SPUTNIK")
-print("2.1 LISTAR CAMPERS DE SPUTNIK")
-print("2.2 AGREGAR CAMPERS A SPUTNIK")
-print("2.3 ELIMINAR CAMPERS DE SPUTNIK")
-print("2.4 ORDENAR ALFABETICAMENTE EN LISTA DE SPUTNIK")
-print("2.5 BUSCAR CAMPER EN LISTA DE SPUTNIK")
+print("----------JUEGOS OLIMPICOS 2022----------")
 
+print("----------NOMBRES DE LOS ATLETAS FINALISTAS----------")
 
-a = float(input("Digite opcion: "))
-if a == 1.:
-    print("-----------------------Crear grupo artemis-----------------------")
-    name = input("Digite el nombre del grupo artemis: ")
-    print(f"El grupo se llama {name}")
+nombre1 = input("Escriba el nombre del atleta 1: ")
+nombre2 = input("Escriba el nombre del atleta 2: ")
+nombre3 = input("Escriba el nombre del atleta 3: ")
 
-elif a == 1.1:
-    print("-----------------------Listar campers de artemis-----------------------")
-    list=['Roberto','Alicia','Daniel','Carlos','Vannessa']
-    print(list)
+print("----------MARCAS DEL SALTO EN METROS DE LOS ATLETAS----------")
 
-elif a == 1.2:
-    print("-----------------------Agregar campers a artemis-----------------------")
-    list=['Roberto','Alicia','Daniel','Carlos','Vannessa']
-    print(list)
-    name = input("Digite el nombre del nuevo camper: ")
-    list.insert(5,(name))
-    print("El camper se ha añadido exitosamente:")
-    print(list)
+salto1 = int(input("Digite el salto del atleta 1: "))
+salto2 = int(input("Digite el salto del atleta 2: "))
+salto3 = int(input("Digite el salto del atleta 3: "))
 
-elif a == 1.3:
-    print("-----------------------Eliminar campers de artemis-----------------------")
-    list=['Roberto','Alicia','Daniel','Carlos','Vannessa']
-    print(list)
-    name = int(input("Digite la posicion del camper que desea eliminar: "))
-    del list[(name)]
-    print("El camper se ha eliminado exitosamente:")
-    print(list)
+if  salto1 > salto2 and salto3:     
+        if salto1 > 15.50:
+                print(f"El atleta {nombre1} ha rompido el record, recibió 500 millones")
+        print(f"GANADOR {nombre1}, ha ganado la medalla de oro")
 
-elif a == 1.4:
-    print("-----------------------Lista de artemis ordenada alfabeticamente-----------------------")
-    list=['Roberto','Alicia','Daniel','Carlos','Vannessa']
-    ordenados = sorted(list)
-    print(ordenados)
+elif salto2 > salto3 and salto1:
+        if salto2 > 15.50:
+                print(f"El atleta {nombre2} ha rompido el record, recibió 500 millones")
+        print(f"GANADOR {nombre2}, ha ganado la medalla de oro")
 
-elif a == 1.5:
-    print("-----------------------Buscar campers en la lista de artemis-----------------------")
-    list=['Roberto','Alicia','Daniel','Carlos','Vannessa']
-    print(list)
-    name = int(input("Digite la posicion del camper que desea buscar: "))
-    print(list[name])
-    print("CAMPER ENCONTRADO")
+elif salto3 > salto1 and salto2:
+        if salto3 > 15.50:
+                print(f"El atleta {nombre3} ha rompido el record, recibió 500 millones")
+        print(f"GANADOR {nombre3}, ha ganado la medalla de oro")
 
-elif a == 2.:
-    print("-----------------------Crear grupo sputnik-----------------------")
-    name = input("Digite el nombre del grupo sputnik: ")
-    print(f"El grupo se llama {name}")
-
-elif a == 2.1:
-    print("-----------------------Listar campers de sputnik-----------------------")
-    list=['Sebastian','Javier','Juan','Andres','Daniel']
-    print(list)
-
-elif a == 2.2:
-    print("-----------------------Agregar campers a sputnik-----------------------")
-    list=['Sebastian','Javier','Juan','Andres','Daniel']
-    print(list)
-    name = input("Digite el nombre del nuevo camper: ")
-    list.insert(5,(name))
-    print("El camper se ha añadido exitosamente:")
-    print(list)
-
-elif a == 2.3:
-    print("-----------------------Eliminar campers de sputnik-----------------------")
-    list=['Sebastian','Javier','Juan','Andres','Daniel']
-    print(list)
-    name = int(input("Digite la posicion del camper que desea eliminar: "))
-    del list[(name)]
-    print("El camper se ha eliminado exitosamente:")
-    print(list)
-
-elif a == 2.4:
-    print("-----------------------Lista de sputnik ordenada alfabeticamente-----------------------")
-    list=['Sebastian','Javier','Juan','Andres','Daniel']
-    ordenados = sorted(list)
-    print(ordenados)
-
-elif a == 2.5:
-    print("-----------------------Buscar campers en la lista de sputnik-----------------------")
-    list=['Sebastian','Javier','Juan','Andres','Daniel']
-    print(list)
-    name = int(input("Digite la posicion del camper que desea buscar: "))
-    print(list[name])
-    print("CAMPER ENCONTRADO")
+else:
+    print("Ha habido un empate entre los atletas")
